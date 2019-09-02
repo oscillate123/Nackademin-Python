c4043846f53a540ed19101d60f928f8ff96f5a80
oscillate123 <neonode2014@gmail.com>
2019-09-02 11:08:29 +0200

    adding exercises

---
+++ w3/8.py
@@ -0,0 +1,13 @@
+"""
+
+8. Write a Python function that takes a list of words and returns the length of the longest one.
+
+"""
+
+word_1 = "testing"
+word_2 = "test"
+
+if len(word_1) > len(word_2):
+    print(word_1)
+else:
+    print(word_2)
