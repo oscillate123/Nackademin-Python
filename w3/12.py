a55d6f838d89fdeff6b3601461553942cffa3017
oscillate123 <neonode2014@gmail.com>
2019-09-02 16:46:07 +0200

    adding exercises

--- w3/12.py
+++ w3/12.py
@@ -2,9 +2,30 @@
 
 12. Write a Python program to count the occurrences of each word in a given sentence.
 
+Re-used the code from exercise 2, and adjusted it for this exercise.
+
+string = "google.com"
+count = {}
+
+for i in string:
+    if i in count:
+        count[i] += 1
+    else:
+        count[i] = 1
+
+print(" Character count:" + "\n" + str(count))
+
 """
 
-sentence = "I have no life and I don't like the letter I"
+sentence = "ding pling sing king ding bing ding"
 values = sentence.split(" ")
-print(values)
 
+count = {}
+
+for item in values:
+    if item in count:
+        count[item] += 1
+    else:
+        count[item] = 1
+
+print(count)
