b9443c40dbbff810c40d3c1ccaf944df13130de6
Oscar J Mac <neonode2014@gmail.com>
2019-09-04 09:33:30 +0200

    adding lecture notes

---
+++ forelasning_5/loop_TEST.py
@@ -0,0 +1,17 @@
+my_numbers = [1, 2, 3, 4, 5, 6, 7]
+
+for i in my_numbers:
+    print(i)
+
+
+print("\n")
+
+y = True
+x = 0
+
+while y:
+    x += 1
+    print(x)
+    if x == 8:
+        y = False
+
