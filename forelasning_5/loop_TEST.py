777a32b97aa321c8e90e4d079169b904270c23d7
Oscar J Mac <neonode2014@gmail.com>
2019-09-04 09:54:45 +0200

    updating lecture notes

--- forelasning_5/loop_TEST.py
+++ forelasning_5/loop_TEST.py
@@ -15,3 +15,35 @@ while y:
     if x == 8:
         y = False
 
+
+print("\n")
+
+x_list = [1,2,3]
+y_list = ["a", "b", "c"]
+
+
+for x in x_list:
+    print(x)
+    for y in y_list:
+        print(str(x) + y)
+
+
+print("\n")
+
+for val in "string":
+    if val != "n":
+        print(val)
+        continue
+    else:
+        break
+
+print(" value == n , therefor we break the loop")
+
+
+print("\n")
+
+numbers = [1, 2, 3, 4, 5, 6, 7]
+
+squares = [number**2 for number in numbers if (number % 2) != 0]
+
+print(squares)
