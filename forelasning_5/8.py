89c76e50d318edb50d8cba01b75eb51d4615820a
oscillate123 <neonode2014@gmail.com>
2019-09-05 21:36:49 +0200

    adding exercise

--- forelasning_5/8.py
+++ forelasning_5/8.py
@@ -5,7 +5,27 @@ skriva ut alla primtal som är mindre än 100.
 Vägledning: Primtal är ett tal som är större än 1 och som inte går att dela
 jämnt med något tal annat än sig själv och 1.
 
-
-
 """
 
+i = 1
+
+while i < 100:
+    # print i if i is prime
+
+    isPrime = True  # flag
+    x = 2
+
+    while (i / 2) >= x:
+        # print(f" i -> {i}")
+        # print(f" x -> {x}")
+
+        if i % x == 0:
+            isPrime = False  # raise flag
+            break
+        x += 1
+
+    if isPrime:  # flag check
+        print(i)
+
+    # print("\n" + "_________")
+    i += 1
