ff11a020ef5f6b431e0f941fc20cc5b7c3815026
Oscar J Mac <neonode2014@gmail.com>
2019-09-06 10:05:51 +0200

    adjusting minor changes, adding stuff

--- forelasning_6/lecture_notes.py
+++ forelasning_6/lecture_notes.py
@@ -17,3 +17,13 @@ def function_name(argument1, argument2, argument3):
 
 print(function_name(1, 2, 3))
 print(argument4)
+
+
+plus = lambda var1, var2, var3: var1 + var2 + var3
+
+minus = lambda x, y, z: x - y - z
+
+print(f"Lambda plus resultat: {plus(2,2,3)}")
+
+print(f"Lambda minus resultat: {minus(6, 3, 1)}")
+
