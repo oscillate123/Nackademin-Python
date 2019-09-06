b91b857d7a61587a72889ce7fc4e35ec597ccc4e
Oscar J Mac <neonode2014@gmail.com>
2019-09-06 10:10:05 +0200

    adding first exercise

---
+++ forelasning_6/1.py
@@ -0,0 +1,25 @@
+numbers = [4, 2]
+
+while True:
+    print(numbers[0] + numbers[1])
+    print(numbers[0] - numbers[1])
+    print(numbers[0] / numbers[1])
+    print(numbers[0] * numbers[1])
+    print(numbers[0] % numbers[1])
+    print(numbers[0] ** numbers[1])
+    break
+
+
+print("\n")
+
+def math_op(val1, val2):
+    print(numbers[0] + numbers[1])
+    print(numbers[0] - numbers[1])
+    print(numbers[0] / numbers[1])
+    print(numbers[0] * numbers[1])
+    print(numbers[0] % numbers[1])
+    print(numbers[0] ** numbers[1])
+    return None
+
+
+math_op(4, 2)
