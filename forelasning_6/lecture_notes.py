3511e49fafbad94375bce7e251741cfac300466e
Oscar J Mac <neonode2014@gmail.com>
2019-09-06 09:41:32 +0200

    adding lecture notes

---
+++ forelasning_6/lecture_notes.py
@@ -0,0 +1,22 @@
+def function_sum(term1, term2):
+    sum = term1 + term2
+    print(f"Sum is {sum}")
+
+
+function_sum(3, 4)
+
+
+def function_name(argument1, argument2, argument3):
+    return_value = argument1 + argument2 + argument3
+
+    global argument4
+    argument4 = 4
+
+    return return_value, argument1, argument2, argument3
+
+
+print(function_name(1, 2, 3))
+print(argument4)
+
+
+tony.twumasi@gmail.com
