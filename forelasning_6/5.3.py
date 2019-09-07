2d17133ae308b99f96f482daa411f2a3213cbb11
Oscar J Mac <neonode2014@gmail.com>
2019-09-07 22:50:14 +0200

    adding last exercise

--- forelasning_6/5.3.py
+++ forelasning_6/5.3.py
@@ -1,38 +1,35 @@
-# val1 = int(input("Please type an integer: "))
-# val2 = int(input("Please type another integer: "))
-#
-# +-/*%**
-#
-# print(f"Resultatet av:\n{val1} + {val2} = {val1 + val2}")
-# print(f"Resultatet av:\n{val1} - {val2} = {val1 - val2}")
-# print(f"Resultatet av:\n{val1} / {val2} = {val1 / val2}")
-# print(f"Resultatet av:\n{val1} * {val2} = {val1 * val2}")
-# print(f"Resultatet av:\n{val1} % {val2} = {val1 % val2}")
-# print(f"Resultatet av:\n{val1} ** {val2} = {val1 ** val2}")
+add = lambda var1, var2: var1 + var2
+sub = lambda var1, var2: var1 - var2
+div = lambda var1, var2: var1 / var2
+mult = lambda var1, var2: var1 * var2
+mod = lambda var1, var2: var1 % var2
 
-arithmetics = ["+", "minus", "divide", "multiply", "mod", "potency"]
+arithmetic = {"+": add,
+               "-": sub,
+               "/": div,
+               "*": mult,
+               "%": mod}
 
-u_var1 = int(input("Please type an integer: "))
-u_arithmetic = input(f"Which arithmetic do you want to use?\nYou can use these:\n{arithmetics}\n")
-u_arithmetic_type = u_arithmetic
-u_var2 = int(input("Please type another integer: "))
+while True:
+    u_var1 = int(input("Please type an integer: "))
 
-if u_arithmetic == "+":
-    u_arithmetic = lambda var1, var2: var1 + var2
-elif u_arithmetic == "minus":
-    u_arithmetic = lambda var1, var2: var1 - var2
-elif u_arithmetic == "divide":
-    u_arithmetic = lambda var1, var2: var1 / var2
-elif u_arithmetic == "multiply":
-    u_arithmetic = lambda var1, var2: var1 * var2
-elif u_arithmetic == "mod":
-    u_arithmetic = lambda var1, var2: var1 % var2
-elif u_arithmetic == "potency":
-    u_arithmetic = lambda var1, var2: var1 ** var2
+    u_arithmetic = input(f"Which arithmetic do you want to use?\n"
+                        f"You can use these:\n"
+                        f"{arithmetic.keys()}\n")
+
+    u_var2 = int(input("Please type another integer: "))
+
+    print(f"Is this correct? {u_var1} {u_arithmetic} {u_var2}")
+    u_confirm = input(f"Yes/No \n ").lower()
+
+    arithmetic_value = arithmetic[u_arithmetic]
+
+    if u_confirm == "yes":
+        break
 
 
 def math_op(lambda_f, v1, v2):
-    print(f"{v1} {u_arithmetic_type} {v2} gives: {lambda_f(v1, v2)}")
+    print(f"{v1} {u_arithmetic} {v2} = {lambda_f(v1, v2)}")
 
 
-math_op(u_arithmetic, u_var1, u_var2)
+math_op(arithmetic_value, u_var1, u_var2)
