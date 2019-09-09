91996dd7c59f5c6c06fe02383bc74ae685a9ad23
oscillate123 <neonode2014@gmail.com>
2019-09-09 15:38:27 +0200

    updating, make it more efficient

--- forelasning_7/numpy_and_pandas/tasks/3.3.py
+++ forelasning_7/numpy_and_pandas/tasks/3.3.py
@@ -1,35 +1,15 @@
-# import random
-# for i in range(0, 12):
-#     x = random.randint(1,10)
-#     num_list.append(x)
-
 import pandas as pd
 import numpy as np
 
+year_list = [2017, 2018, 2019]
+quarter_list = ["Q1", "Q2", "Q3", "Q4"]
+
 num_list = []
 
-QX = 1
+for year in year_list:
+    for quarter in quarter_list:
+        num_list.append(int(input(f"What was the result of {quarter} {year}? : ")))
 
-for i in range(1, 13):
-
-    if i < 5:
-        year = 2017
-        # QX = quarter(i)
-        num_list.append(int(input(f"Vad var resultatet för Q{QX} {year}?")))
-        QX += 1
-
-    elif 4 < i < 9:
-        year = 2018
-        # QX = quarter(i)
-        num_list.append(int(input(f"Vad var resultatet för Q{QX} {year}?")))
-        QX += 1
-
-    elif 8 < i:
-        year = 2019
-        # QX = quarter(i)
-        num_list.append(int(input(f"Vad är resultatet för Q{QX} {year}?")))
-        QX += 1
-
-pd_df = pd.DataFrame(np.array([num_list]).reshape(3, 4), columns=["Q1", "Q2", "Q3", "Q4"], index=[2017, 2018, 2019])
+pd_df = pd.DataFrame(np.array([num_list]).reshape(3, 4), columns=quarter_list, index=year_list)
 
 print(pd_df)
