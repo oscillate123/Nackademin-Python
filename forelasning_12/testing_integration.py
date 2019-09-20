209f593e512df2556013e8c7c318ae456384a4b5
Oscar J Mac <neonode2014@gmail.com>
2019-09-20 14:25:07 +0200

    updating

---
+++ forelasning_12/testing_integration.py
@@ -0,0 +1,12 @@
+import forelasning_8.personnummer_simon as pnummer_control
+import unittest
+
+
+class TestCase(unittest.TestCase):
+
+    def fixFormat(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
--- forelasning_12/testing_unit.py
+++ forelasning_12/testing_unit.py
@@ -5,14 +5,12 @@ import unittest
 class TestCase(unittest.TestCase):
 
     def test_strip_century(self):
-        p_number = "9708232898"
+        p_number = pnummer_control.user_input()
         the_func = pnummer_control.stripCentury(number=p_number)
         if len(p_number) > 10:
-            self.assertEqual(len(the_func), len(p_number)-2, msg="Function not working")
+            self.assertEqual(len(the_func), len(p_number)-4, msg="Function not working")
         elif len(p_number) < 11:
             self.assertEqual(len(the_func), len(p_number), msg="Function not working")
-
-        return p_number
 
 
 if __name__ == '__main__':
